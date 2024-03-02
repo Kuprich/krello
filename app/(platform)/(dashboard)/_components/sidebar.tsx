67e6f99a57a032a6913234828ae24c9d4ab402da
kuprich <ilya-kyp@yandex.ru>
2024-03-02 17:53:50 +0300

    sidebar created

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -7,6 +7,7 @@ import { useOrganization, useOrganizationList } from "@clerk/nextjs"
 import { Key, Plus } from "lucide-react"
 import Link from "next/link"
 import { useLocalStorage } from "usehooks-ts"
+import { NavItem, Organization } from "./nav-item"
 
 interface SidebarProps {
   storageKey?: string
@@ -78,9 +79,13 @@ export const Sidebar = ({
         className="space-y-2"
       >
         {userMemberships.data.map(({organization}) => (
-          <p key={organization.id}>
-            {organization.id}
-          </p>
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
         ))}
 
       </Accordion>
