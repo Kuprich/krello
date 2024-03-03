eb00a5a88bfbaaecf2c9f4da757e8f19393d8d8c
kuprich <ilya-kyp@yandex.ru>
2024-03-03 17:20:59 +0300

    workspace settings

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -4,7 +4,7 @@ import { Accordion } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
 import { useOrganization, useOrganizationList } from "@clerk/nextjs"
-import { Key, Plus } from "lucide-react"
+import { Plus } from "lucide-react"
 import Link from "next/link"
 import { useLocalStorage } from "usehooks-ts"
 import { NavItem, Organization } from "./nav-item"
@@ -51,7 +51,15 @@ export const Sidebar = ({
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     )
   }
@@ -78,7 +86,7 @@ export const Sidebar = ({
         defaultValue={defaultAccordionValue}
         className="space-y-2"
       >
-        {userMemberships.data.map(({organization}) => (
+        {userMemberships.data.map(({ organization }) => (
           <NavItem
             key={organization.id}
             isActive={activeOrganization?.id === organization.id}
