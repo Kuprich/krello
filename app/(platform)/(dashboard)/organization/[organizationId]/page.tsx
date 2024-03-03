b900ae309251cf9cb51421799b84eeb4d804f452
kuprich <ilya-kyp@yandex.ru>
2024-03-03 21:19:35 +0300

    create Board model

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,18 @@
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
+
 const OrganizationPage = () => {
   return (
     <div>
-      Organization Page
+      <form action={create}>
+        <input 
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-2 text-sm"/>
+          <Button type="submit">Submit</Button>
+      </form>
     </div>
   )
 }
